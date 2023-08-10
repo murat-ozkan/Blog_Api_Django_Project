50c3eefb2b9348fb7cc544546d4dbc9a16c3d1a2
muratozkan79 <muratozkan79@gmail.com>
2023-08-11 01:48:31 +0300

    admin.py and verbose names

--- blog/models.py
+++ blog/models.py
@@ -4,16 +4,20 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Category(models.Model):
-    name = models.CharField(max_length=64)
+    name = models.CharField(verbose_name="Category Names",max_length=64)
+    
+    class Meta:
+        verbose_name = "Blog Category"
+        verbose_name_plural = "Blog Categories"
 
     def __str__(self):
         return self.name
     
 class Post(models.Model):
-    title = models.CharField(max_length=64)
+    title = models.CharField(max_length=128)
     content = models.TextField()
-    created_date = models.DateTimeField()
-    updated_date = models.DateTimeField()
+    created_date = models.DateTimeField(verbose_name="Create Date(Optional)",auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
     category = models.ForeignKey(Category, on_delete = models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
