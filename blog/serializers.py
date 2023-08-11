e2db8e7c545690468a65356260bbb44c7555fa2e
muratozkan79 <muratozkan79@gmail.com>
2023-08-12 02:49:45 +0300

    serializers and pagination

--- blog/serializers.py
+++ blog/serializers.py
@@ -7,6 +7,12 @@ class CategorySerializer(serializers.ModelSerializer):
         exclude = []
 
 class PostSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField() # Bu 2 satır yazılmadan önce user ve category 1 olarak geliyordu. İsim gelmiyordu.
+    category = serializers.StringRelatedField() # Bu 2 satır sayedinde ilişkisel tablodan veriyi aldı. Yani class Category self.name
+
+    user_id = serializers.IntegerField() # Bu 2 satır da user_id ve category_id için gerekli.
+    category_id = serializers.IntegerField()
+
     class Meta:
         model = Post
         exclude = []
