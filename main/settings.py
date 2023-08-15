25978c04b9a6ca65e4722c7b045a462eeda66add
muratozkan79 <muratozkan79@gmail.com>
2023-08-15 22:15:19 +0300

    User password validaiton and hashing

--- main/settings.py
+++ main/settings.py
@@ -130,9 +130,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Pagination
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.TokenAuthentication'],
-    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticatedOrReadOnly'],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.TokenAuthentication'],
+    # 'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.IsAuthenticatedOrReadOnly'],
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
     # 'DEFAULT_FILTER_BACKENDS': [
     #   'django_filters.rest_framework.DjangoFilterBackend', 
--- user/serializers.py
+++ user/serializers.py
@@ -11,3 +11,15 @@ class UserSerializer(serializers.ModelSerializer):
             "last_login",
             "user_permissions"
             ]
+    
+    def validate(self, attrs):
+        from django.contrib.auth.password_validation import validate_password # doğrulama fonksiyonu
+        from django.contrib.auth.hashers import make_password # şifreleme fonksiyonu
+        password = attrs['password'] # Password al.
+        validate_password(password) # Validation'dan geçir.
+        attrs.update(
+            {
+                'password': make_password(password) # Password şifrele ve güncelle.
+            }
+        )
+        return super().validate(attrs) # Orjinal methodu çalıştır.
