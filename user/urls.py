6364bdd8516c9b0fa0b765d9ab6802bc2fbaf9ee
muratozkan79 <muratozkan79@gmail.com>
2023-08-21 22:46:13 +0300

    Log Out

--- user/urls.py
+++ user/urls.py
@@ -1,15 +1,23 @@
-from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from user.views import UserView
+from rest_framework.routers import DefaultRouter
+from .views import UserView
 from rest_framework.authtoken.views import obtain_auth_token
 
-router = DefaultRouter()
-router.register("", UserView)
+# ----------------------------------------------------------------
+# Logout function:
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+@api_view(['GET', 'POST'])
+def logout(request):
+    request.user.auth_token.delete() # Token Sil.
+    return Response({"message": 'User Logout: Token Deleted'})
+# ----------------------------------------------------------------
 
-# urlpatterns = router.urls
-# veya
+router = DefaultRouter()
+router.register('', UserView)
+
 urlpatterns = [
     path('', include(router.urls)),
     path('login', obtain_auth_token),
-    # path('logout', logout),
+    path('logout', logout),
 ]
